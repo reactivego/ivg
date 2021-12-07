35680395c2bd78c3f5e90136430879551b52b645
René Post <git@xpt.nl>
2021-12-07 08:21:33 +0100

    Remove comment

--- render/gradient.go
+++ render/gradient.go
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package gradient provides linear and radial gradient images.
-
 package render
 
 import (
